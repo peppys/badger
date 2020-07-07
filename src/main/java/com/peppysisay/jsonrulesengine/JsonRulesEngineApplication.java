7dfcfdd958582c27f493f44eb44613486f7339de
Peppy Sisay <peppy@dave.com>
2020-07-07 07:18:00 -0700

    Initial setup

---
+++ src/main/java/com/peppysisay/jsonrulesengine/JsonRulesEngineApplication.java
@@ -0,0 +1,11 @@
+package com.peppysisay.jsonrulesengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonRulesEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JsonRulesEngineApplication.class, args);
+    }
+}
---
+++ src/test/java/com/peppysisay/jsonrulesengine/JsonRulesEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peppysisay.jsonrulesengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonRulesEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
